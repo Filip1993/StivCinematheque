f49b3372a2a0176044f7152e6619692671ec0b87
daniel bele <dnlbele@gmail.com>
2015-04-14 14:09:28 +0200

    getUri fixed

--- app/src/main/java/com/dbele/stiv/persistence/MoviesContentProvider.java
+++ app/src/main/java/com/dbele/stiv/persistence/MoviesContentProvider.java
@@ -22,26 +22,26 @@ public class MoviesContentProvider extends ContentProvider {
     private static final String BASE_PATH = "movies";
     public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH);
 
-//    public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/movies";
-//    public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/movie";
+    public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/movies";
+    public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/movie";
 
-    private static final UriMatcher sURIMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    private static final UriMatcher URI_MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
 
-    public static final String[] MOVIES_LIST_PROJECTION = { MovieDatabaseHelper.COLUMN_ID, MovieDatabaseHelper.COLUMN_NAME,
-            MovieDatabaseHelper.COLUMN_WATCHED, MovieDatabaseHelper.COLUMN_PICTURE_PATH };
+    public static final String[] MOVIES_LIST_PROJECTION = {MovieDatabaseHelper.COLUMN_ID, MovieDatabaseHelper.COLUMN_NAME,
+            MovieDatabaseHelper.COLUMN_WATCHED, MovieDatabaseHelper.COLUMN_PICTURE_PATH};
 
-    public static final String[] MOVIE_PROJECTION = { MovieDatabaseHelper.COLUMN_ID, MovieDatabaseHelper.COLUMN_NAME,
+    public static final String[] MOVIE_PROJECTION = {MovieDatabaseHelper.COLUMN_ID, MovieDatabaseHelper.COLUMN_NAME,
             MovieDatabaseHelper.COLUMN_DESCRIPTION, MovieDatabaseHelper.COLUMN_DIRECTOR, MovieDatabaseHelper.COLUMN_ACTORS,
             MovieDatabaseHelper.COLUMN_LENGTH, MovieDatabaseHelper.COLUMN_GENRE, MovieDatabaseHelper.COLUMN_PICTURE_PATH,
             MovieDatabaseHelper.COLUMN_TICKET_PATH, MovieDatabaseHelper.COLUMN_IMPRESSIONS, MovieDatabaseHelper.COLUMN_WATCHED,
-            MovieDatabaseHelper.COLUMN_ARCHIVED, MovieDatabaseHelper.COLUMN_WATCHED_DATE };
+            MovieDatabaseHelper.COLUMN_ARCHIVED, MovieDatabaseHelper.COLUMN_WATCHED_DATE};
 
-    public static final String[] MOVIE_NAMES_PROJECTION = { MovieDatabaseHelper.COLUMN_NAME };
+    public static final String[] MOVIE_NAMES_PROJECTION = {MovieDatabaseHelper.COLUMN_NAME};
 
 
     static {
-        sURIMatcher.addURI(AUTHORITY, BASE_PATH, MOVIES);
-        sURIMatcher.addURI(AUTHORITY, BASE_PATH + "/#", MOVIE_ID);
+        URI_MATCHER.addURI(AUTHORITY, BASE_PATH, MOVIES);
+        URI_MATCHER.addURI(AUTHORITY, BASE_PATH + "/#", MOVIE_ID);
     }
 
 
@@ -58,7 +58,7 @@ public class MoviesContentProvider extends ContentProvider {
         SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
 
         queryBuilder.setTables(MovieDatabaseHelper.TABLE_NAME);
-        int uriType = sURIMatcher.match(uri);
+        int uriType = URI_MATCHER.match(uri);
         switch (uriType) {
             case MOVIES:
                 break;
@@ -80,7 +80,7 @@ public class MoviesContentProvider extends ContentProvider {
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        int uriType = sURIMatcher.match(uri);
+        int uriType = URI_MATCHER.match(uri);
         SQLiteDatabase sqlDB = dbHelper.getWritableDatabase();
         long id = 0;
         switch (uriType) {
@@ -96,7 +96,7 @@ public class MoviesContentProvider extends ContentProvider {
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        int uriType = sURIMatcher.match(uri);
+        int uriType = URI_MATCHER.match(uri);
         SQLiteDatabase sqlDB = dbHelper.getWritableDatabase();
         int rowsDeleted = 0;
         switch (uriType) {
@@ -107,30 +107,33 @@ public class MoviesContentProvider extends ContentProvider {
             case MOVIE_ID:
                 String id = uri.getLastPathSegment();
                 rowsDeleted = sqlDB.delete(MovieDatabaseHelper.TABLE_NAME,
-                        MovieDatabaseHelper.COLUMN_ID + "=" + id, selectionArgs );
+                        MovieDatabaseHelper.COLUMN_ID + "=" + id, selectionArgs);
                 break;
             default:
                 throw new IllegalArgumentException("Unknown URI: " + uri);
         }
         getContext().getContentResolver().notifyChange(uri, null);
-        return rowsDeleted;    }
+        return rowsDeleted;
+    }
 
 
     @Override
     public String getType(Uri uri) {
-        // TODO: Implement this to handle requests for the MIME type of the data
-        // at the given URI.
-        throw new UnsupportedOperationException("Not yet implemented");
+        switch (URI_MATCHER.match(uri)) {
+            case MOVIES:
+                return CONTENT_TYPE;
+            case MOVIE_ID:
+                return CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
     }
-
-
-
 
 
     @Override
     public int update(Uri uri, ContentValues values, String selection,
                       String[] selectionArgs) {
-        int uriType = sURIMatcher.match(uri);
+        int uriType = URI_MATCHER.match(uri);
         SQLiteDatabase sqlDB = dbHelper.getWritableDatabase();
         int updateCount = 0;
         switch (uriType) {
